72b7cc8ad83fb0b109fe77f58afb2133a99cd807
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-10-20 15:09:01 +0300

    Added styles and new layout for index.js

---
+++ components/layout.js
@@ -0,0 +1,82 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
+const name = 'CodingOnMars';
+export const siteTitle = 'Next.js Sample Website';
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+        <meta
+          property='og:image'
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name='og:title' content={siteTitle} />
+        <meta name='twitter:card' content='summary_large_image' />
+      </Head>
+      <header className={styles.header}>
+        {/* Adjust size of image and title */}
+        {home ? (
+          <>
+            <Image
+              priority
+              src='/images/profile-picture.png'
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href='/'>
+              <a>
+                <Image
+                  priority
+                  src='/images/profile-picture.png'
+                  className={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href='/'>
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {/* Show back to home link if you are not at the home page */}
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href='/'>
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+// SECTION: Theory notes
+
+/* NOTES:
+
+1. Images added with <Image/> component from next/image
+
+Priority for images means that they will be preloaded with high priority.
+Lazy loading is disabled for images using priority.
+
+*/
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,19 +1,26 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First post</h1>
       <h2>
         <Link href='/'>
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
 
-// SECTION: Theory notes
+// Theory notes
+
+// SECTION: Navigate between pages
 
 /* NOTES:
 1. To link pages in Next.js we wrap <a></a> tag to Link component like this:
@@ -38,5 +45,58 @@ Each page loads only what necessary for the page.
 Pages are isolated. If one fails, others will work.
 
 In production build Next.js uses prefetch to load linked pages in the background (like Chrome does).
+
+*/
+
+// SECTION: Assets, metadata, CSS
+
+/* NOTES:
+
+1. Static assets go to public folder. This also includes robots.txt.
+
+2. Images.
+
+Next.js perks out of a box:
+
+- responsive images;
+- auto optimising images*;
+- lazy loading by default (hmmm, it's not necessary mean that it will be fast or save data)
+
+* - What is cool Next.js serves images in modern formats if a browser supports them;
+  - Work with any image source, including CMS.
+
+3. Metadata
+
+In Next.js built-in <Head></Head> component is used instead of <head></head>
+
+To customize html tag, for example, to add lang='', 
+we need to create a separate _document.js file under pages folder
+
+LINK: https://nextjs.org/docs/advanced-features/custom-document 
+
+4. CSS Styling
+
+Next.js has built-in support for CSS (with modules!), Sass, styled-jsx (meh).
+
+CSS Modules
+
+NOTE: to use CSS modules, CSS file name must end with .module.css
+
+If we use CSS modules, class names will be generated automatically. 
+So we don't have to worry about names collisions. 
+
+Next.js code splitting works on CSS modules, loading only minimal CSS for each page. 
+
+Next.js generate css files from modules and load them automatically in production.
+
+Global styles
+
+To load global CSS file, we need to create _app.js with App component under pages folder.
+
+App component is common across different pages.
+
+We need to restart dev server after adding _app.js.
+
+NOTE: we can import global CSS files only inside pages/_app.js
 
 */
